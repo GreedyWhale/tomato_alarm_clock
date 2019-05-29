a8d2602355969880c8168deea823e99d33b549a0
caisr <383911973@qq.com>
2019-05-29 15:35:07 +0800

    更新README.md && 增加部署流程

--- src/methods/history/index.ts
+++ src/methods/history/index.ts
@@ -1,5 +1,14 @@
 import { createBrowserHistory } from "history";
 
-const customHistory = createBrowserHistory();
+const env = process.env.NODE_ENV;
+let publicUrl: string =  '';
 
+if (env === 'development') {
+  publicUrl = '/';
+} else if (env === 'production') {
+  publicUrl = '/tomato_alarm_clock';
+}
+const customHistory = createBrowserHistory({
+  basename: publicUrl
+});
 export default customHistory;
